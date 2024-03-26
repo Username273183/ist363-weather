f75961cdcc56526c0390f305a84a38586e175bdd
Omar Camara <117489682+Username273183@users.noreply.github.com>
2024-03-26 18:20:17 -0400

    attendance 03/26/24 Used State

--- app/page.js
+++ app/page.js
@@ -1,11 +1,43 @@
+"use client";
+
+import { useState } from "react";
+
 import Button from "../components/Button";
+import ColorPicker from "../components/ColorPicker";
 const Homepage = () => {
+  const [count, setCount] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div>
       <h1>Weather App</h1>
+      <h2>Count: {count}</h2>
+      <Button
+        label="Increment"
+        clickHandler={() => {
+          setCount(count + 1);
+        }}
+      />
+      <Button
+        label="Decrement"
+        clickHandler={() => {
+          setCount(count - 1);
+        }}
+      />
       <Button label="Download" />
       <Button label="Register now" />
       <Button label="Learn more" />
+
+      {count > 5 && <div> Special message</div>}
+      <br />
+      <Button
+        label={isVisible ? "Hide Message" : "Show Message"}
+        clickHandler={() => {
+          setIsVisible(!isVisible);
+        }}
+      />
+      {isVisible && <p>Hello, World!</p>}
+
+      <ColorPicker />
     </div>
   );
 };
